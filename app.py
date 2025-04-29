42e16077d2f4339fa72e945156d3e05e43c3815a
sevvalsagir <sevvalsgr39@gmail.com>
2025-04-29 11:49:25 +0200

    new folders

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import torch
 import torchvision.transforms as transforms
@@ -11,15 +12,10 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = models.resnet18(weights=None)
 model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-model.fc = torch.nn.Linear(model.fc.in_features, 8)
+model.fc = torch.nn.Linear(model.fc.in_features, 3)  # 3 sınıf için
 model = model.to(device)
 
-import os
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(CURRENT_DIR, "radar_resnet18_model.pth")
-
-
-model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
+model.load_state_dict(torch.load("radar_resnet18_model.pth", map_location=device))
 model.eval()
 
 # Transform
@@ -38,7 +34,6 @@ st.set_page_config(page_title="TEGIN AI Radar Target Classifier", layout="center
 st.title("🎯 TEGIN AI Radar Target Classifier")
 
 if st.button("🚀 Get New Radar Image"):
-    # Randomly select an image
     selected_class = random.choice(classes)
     class_folder = os.path.join("data/mstar", selected_class)
     selected_image_path = os.path.join(class_folder, random.choice(os.listdir(class_folder)))
@@ -47,13 +42,11 @@ if st.button("🚀 Get New Radar Image"):
     image_display = image.copy()
     image = transform(image).unsqueeze(0).to(device)
 
-    # Prediction
     with torch.no_grad():
         output = model(image)
         _, prediction = torch.max(output, 1)
         predicted_class = classes[prediction.item()]
 
-    # Show on page
     st.image(image_display, caption="Radar Image", width=400)
     st.markdown(f"**True Class:** `{selected_class}`")
     st.markdown(f"**TEGIN AI Prediction:** `{predicted_class}`")
